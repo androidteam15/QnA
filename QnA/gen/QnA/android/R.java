e74f4cd3b9ae7eb8daa8719b06a9fbfb80cf345b
androidteam15 <jjeett15@gmail.com>
2013-01-08 02:31:33 +0900

    ddddddddddddd

--- QnA/gen/QnA/android/R.java
+++ QnA/gen/QnA/android/R.java
@@ -16,24 +16,46 @@ public final class R {
         public static final int a3=0x7f020002;
         public static final int cho=0x7f020003;
         public static final int ic_launcher=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int pwn_goskych=0x7f020006;
-        public static final int sang=0x7f020007;
-        public static final int today2_listbar2=0x7f020008;
-        public static final int woo=0x7f020009;
+        public static final int ic_pulltorefresh_arrow=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int pwn_goskych=0x7f020007;
+        public static final int sang=0x7f020008;
+        public static final int today2_listbar2=0x7f020009;
+        public static final int woo=0x7f02000a;
     }
     public static final class id {
-        public static final int ImageView01=0x7f050001;
-        public static final int LinearLayout01=0x7f050000;
-        public static final int elv_list=0x7f05000a;
-        public static final int groupimage=0x7f050003;
-        public static final int iv_image=0x7f050005;
-        public static final int lec=0x7f050009;
-        public static final int qna=0x7f050008;
-        public static final int tvChild=0x7f050002;
-        public static final int tvGroup=0x7f050004;
-        public static final int tv_child=0x7f050007;
-        public static final int tv_group=0x7f050006;
+        public static final int ImageView01=0x7f050002;
+        public static final int LinearLayout01=0x7f050001;
+        public static final int ask=0x7f050017;
+        public static final int book=0x7f050014;
+        public static final int book1=0x7f05001d;
+        public static final int elv_list=0x7f050010;
+        public static final int find=0x7f05001a;
+        public static final int groupimage=0x7f050004;
+        public static final int input=0x7f050019;
+        public static final int iv_image=0x7f050006;
+        public static final int lec=0x7f05000a;
+        public static final int linearLayout1=0x7f050016;
+        public static final int linearLayout2=0x7f050011;
+        public static final int number=0x7f050012;
+        public static final int number1=0x7f05001b;
+        public static final int pull_to_refresh_header=0x7f05000b;
+        public static final int pull_to_refresh_image=0x7f05000d;
+        public static final int pull_to_refresh_progress=0x7f05000c;
+        public static final int pull_to_refresh_text=0x7f05000e;
+        public static final int pull_to_refresh_updated_at=0x7f05000f;
+        public static final int q_list=0x7f05001f;
+        public static final int qna=0x7f050009;
+        public static final int spinner1=0x7f050018;
+        public static final int subject=0x7f050000;
+        public static final int title=0x7f050015;
+        public static final int title1=0x7f05001e;
+        public static final int tvChild=0x7f050003;
+        public static final int tvGroup=0x7f050005;
+        public static final int tv_child=0x7f050008;
+        public static final int tv_group=0x7f050007;
+        public static final int writer=0x7f050013;
+        public static final int writer1=0x7f05001c;
     }
     public static final class layout {
         public static final int askquestion=0x7f030000;
@@ -42,11 +64,17 @@ public final class R {
         public static final int lec=0x7f030003;
         public static final int list_item=0x7f030004;
         public static final int main=0x7f030005;
-        public static final int qna=0x7f030006;
-        public static final int showqestion=0x7f030007;
+        public static final int pull_to_refresh_header=0x7f030006;
+        public static final int qna=0x7f030007;
+        public static final int question_list_item=0x7f030008;
+        public static final int showqestion=0x7f030009;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int pull_to_refresh_pull_label=0x7f040002;
+        public static final int pull_to_refresh_refreshing_label=0x7f040004;
+        public static final int pull_to_refresh_release_label=0x7f040003;
+        public static final int pull_to_refresh_tap_label=0x7f040005;
     }
 }
